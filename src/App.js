d84143e60022b0b728f739cfc757b80ca57ebac3
Anu Mahat <anumahat@hotmail.com>
2024-02-28 22:34:17 +1030

    first commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h4>Anu's First React Application</h4>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
