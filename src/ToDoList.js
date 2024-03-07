5cb3fe8552896196f8217e5dc32d2e7170733a77
Anu Mahat <anumahat@hotmail.com>
2024-03-07 11:12:52 +1030

    Curd function in To do list

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,33 +1,65 @@
 import React, {useState} from "react";
 
+const Todo = ({ todo, onDelete, onToggle }) => {
+  return (
+    <div>
+      <input 
+        type="checkbox" 
+        checked={todo.completed} 
+        onChange={() => onToggle(todo.id)} 
+      />
+      <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+        {todo.text}
+      </span>
+      <button onClick={() => onDelete(todo.id)}>Delete</button>
+    </div>
+  );
+};
 
-function TodoList(){
-    const [todos, setTodos] = useState([]);
-    const [inputValue, setInputValue] = useState('');
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-    const handleAddTodo = ()  => {
-        if (inputValue.trim() !== ''){
-            setTodos([...todos, inputValue]);
-            setInputValue('');
-        }
-    };
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { id: Date.now(), text: newTodo, completed: false }]);
+      setNewTodo('');
+    }
+  };
 
-    return (
-        <div>
-          <input
-            type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <button onClick={handleAddTodo}>Add</button>
-          <ul>
-            {todos.map((todo, index) => (
-              <li key={index}>{todo}</li>
-            ))}
-          </ul>
-        </div>
-      );
-}
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const toggleTodo = id => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <div>
+      <input 
+        type="text" 
+        value={newTodo} 
+        onChange={e => setNewTodo(e.target.value)} 
+        placeholder="Add new todo" 
+      />
+      <button onClick={addTodo}>Add</button>
+      {todos.map(todo => (
+        <Todo 
+          key={todo.id} 
+          todo={todo} 
+          onDelete={deleteTodo} 
+          onToggle={toggleTodo} 
+        />
+      ))}
+    </div>
+  );
+};
+
 
 
 export default TodoList;
